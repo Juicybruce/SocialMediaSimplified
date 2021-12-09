eb817b35baa1bd1df735f40c5417d0fb562099b5
adrew <alex.drew@utas.edu.au>
2021-12-09 12:30:41 +1100

    implemented responsive hamburger menu + refactor

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,7 @@
 console.log('hello world');
+
+$(".header__menu").click(function() {
+  console.log('clicked on menu button')
+  $(".header__menu").toggleClass(" active");
+  $(".header__sidebar").toggleClass(" active");
+});
