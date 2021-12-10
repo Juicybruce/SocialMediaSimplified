57bc4c2913bdf13b70ea70c1b6dd9b15dfe588de
adrew <alex.drew@utas.edu.au>
2021-12-10 13:08:00 +1100

    slick carousel implemnted refactored for better code styling changes to media query code for mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,70 +1,72 @@
-console.log('hello world');
+$(document).ready(function(){
+  console.log('hello world');
 
-$(".header__menu").click(function() {
-  $(".header__menu").toggleClass(" active");
-  $(".header__sidebar").toggleClass(" active");
-});
+  $(".header__menu").click(function() {
+    $(".header__menu").toggleClass(" active");
+    $(".header__sidebar").toggleClass(" active");
+  });
 
-//first screen buttons
-$(".firstScreen__contact").hover(function() {
-  $(this).toggleClass(" active");
-})
+  //first screen buttons
+  $(".first-screen__contact").hover(function() {
+    $(this).toggleClass(" active");
+  })
 
-$(".firstScreen__arrowButton").hover(function() {
-  $(this).toggleClass(" active");
-})
+  $(".first-screen__arrowButton").hover(function() {
+    $(this).toggleClass(" active");
+  })
 
-$(".firstScreen__arrowButton").click(function() {
-  $([document.documentElement, document.body]).animate({
-    scrollTop: $(".secondSection").offset().top
-  }, 1000);
-});
+  $(".first-screen__arrowButton").click(function() {
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $(".second-section").offset().top
+    }, 1000);
+  });
 
-$(".firstScreen__learn").hover(function() {
-  $(this).toggleClass(" active");
-})
+  $(".first-screen__learn").hover(function() {
+    $(this).toggleClass(" active");
+  })
 
-//fourth section arrow
-$(".fourthSection__arrowButton").hover(function() {
-  $(this).toggleClass(" active");
-})
+  //fourth section arrow
+  $(".fourth-section__arrowButton").hover(function() {
+    $(this).toggleClass(" active");
+  })
 
-$(".fourthSection__arrowButton").click(function() {
-  $([document.documentElement, document.body]).animate({
-    scrollTop: $(".fifthSection").offset().top
-  }, 1000);
-});
+  $(".fourth-section__arrowButton").click(function() {
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $(".fifth-section").offset().top
+    }, 1000);
+  });
 
-//project navigation button
-$(".thirdSection__seeMore").hover(function() {
-  $(this).toggleClass(" active");
-})
+  //project navigation button
+  $(".third-section__seeMore").hover(function() {
+    $(this).toggleClass(" active");
+  })
 
-//sixth screen link button
-$(".sixthSection__learn").hover(function() {
-  $(this).toggleClass(" active");
-})
+  //sixth screen link button
+  $(".sixth-section__learn").hover(function() {
+    $(this).toggleClass(" active");
+  })
 
-//testimonials quotes slider
-$(".testimonials__arrowLeft").click(function() {
-  $(".testimonials__quotesContainer ul").animate({left: '-100%'}, 500);
-})
+  //testimonials quotes slider
+  $(".testimonials__quotesContainer").slick({
+    dots: false,
+    mobileFirst: true,
+    prevArrow: ".testimonials__arrow-left",
+    nextArrow: ".testimonials__arrow-right"
+  })
 
-/*$(".testimonials__arrowLeft").click(function() {
-  $(".testimonials__quotesContainer ul").animate({left: '+100%'}, 500);
-})
-*/
-//contact form functionality
-$(".contact__submitButton").hover(function() {
-  $(this).toggleClass(" active");
-})
 
-$(".contact__submitButton").hover(function() {
-  $(this).toggleClass(" active");
-})
+  //contact form functionality
+  $(".contact__submitButton").hover(function() {
+    $(this).toggleClass(" active");
+  })
 
-//removes the active styling from the selected buttons and clears inputs
-$(".contact__submitButton").click(function() {
-  $(".contact__button").removeClass(" active");
-  $(".contact__nameInput input").val('');
+  $(".contact__submitButton").hover(function() {
+    $(this).toggleClass(" active");
+  })
+
+  //removes the active styling from the selected buttons and clears inputs
+  $(".contact__submitButton").click(function() {
+    $(".contact__button").removeClass(" active");
+    $(".contact__nameInput input").val('');
+  })
 })
